262cd407e971dec5b8450613e2373ec36a3a62bb
srijan703 <srijanpokhrel343@gmail.com>
2024-08-26 17:03:21 +0545

    outside telegram loading bug fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import BottomMenu from './BottomMenu';
+import LoadingPage from './LoadingPage';
 
 const LayoutContainer = styled.div`
   font-family: 'Arial, sans-serif';
@@ -30,6 +32,8 @@ function Layout({ children }) {
   const [showBottomMenu, setShowBottomMenu] = useState(false);
   const [menuVisible, setMenuVisible] = useState(false);
   const [restricted, setRestricted] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const isLocalhost = window.location.hostname === 'localhost';
@@ -37,7 +41,8 @@ function Layout({ children }) {
 
     if (isLocalhost) {
       console.log('Running on localhost:3000');
-      setShowBottomMenu(true); // Allow localhost for development purposes
+      setShowBottomMenu(true);
+      setLoading(false); // Allow localhost and stop loading
     } else if (tg && tg.initDataUnsafe && tg.initDataUnsafe.user) {
       const platform = tg.platform;
 
@@ -45,13 +50,16 @@ function Layout({ children }) {
         console.log('Confirmed: Running inside Telegram mobile app');
         tg.expand(); // Expand to fullscreen, hiding the Telegram UI elements
         setShowBottomMenu(true); // Show the bottom menu if in Telegram mobile app
+        setLoading(false); // Stop loading
       } else {
         console.log('Restricted: Running on Telegram Desktop or Web');
         setRestricted(true); // Restrict access if not on mobile app
+        setLoading(false); // Stop loading
       }
     } else {
       console.log('Not confirmed: Running outside Telegram');
-      setRestricted(true); // Restrict access if not in Telegram or on localhost
+      setLoading(true); // Continue showing the loading page if outside Telegram
+      navigate('/'); // Ensure the user stays on the loading page
     }
 
     // Delay showing the menu for 4 seconds
@@ -60,7 +68,11 @@ function Layout({ children }) {
     }, 4000);
 
     return () => clearTimeout(menuTimer); // Clean up the timer if the component unmounts
-  }, []);
+  }, [navigate]);
+
+  if (loading) {
+    return <LoadingPage />; // Show the loading page if still loading or outside Telegram
+  }
 
   if (restricted) {
     return (
