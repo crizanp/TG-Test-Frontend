346718ed9d239382e5408363c1e80f4c884a341b
srijan703 <srijanpokhrel343@gmail.com>
2024-08-30 11:20:11 +0545

    task page fix 7

--- src/utils/getUserID.js
+++ src/utils/getUserID.js
@@ -1,32 +1,30 @@
 import axios from 'axios';
 
-export const getUserID = async (setUserID, setUsername) => {
-  // Fetch userID and username from Telegram
+export const getUserID = async (setUserID) => {
+  // Fetch userID from Telegram
   let tgUserID = window.Telegram.WebApp?.initDataUnsafe?.user?.id;
-  let tgUsername = window.Telegram.WebApp?.initDataUnsafe?.user?.username;
 
-  if (tgUserID && tgUsername) {
-    tgUserID = tgUserID.toString().slice(0, 8);
+  if (tgUserID) {
+    // Take only the first 8 characters of the Telegram userID
+    tgUserID = tgUserID.toString();
     setUserID(tgUserID);
-    setUsername(tgUsername);
 
     try {
       // Try to fetch the user's data from the backend
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/user-info/${tgUserID}`);
-      return { userID: tgUserID, username: tgUsername };
+      return tgUserID;
     } catch (error) {
       if (error.response && error.response.status === 404) {
         // If the user is not found, create a new user on the backend
         try {
           const newUserResponse = await axios.post(`${process.env.REACT_APP_API_URL}/user-info/`, {
             userID: tgUserID,
-            username: tgUsername,
             points: 0,
             tasksCompleted: [],
             taskHistory: [],
           });
           console.log('New user created:', newUserResponse.data);
-          return { userID: tgUserID, username: tgUsername };
+          return tgUserID;
         } catch (postError) {
           console.error('Error creating new user:', postError);
           throw postError;  // Rethrow the error if user creation fails
@@ -37,7 +35,9 @@ export const getUserID = async (setUserID, setUsername) => {
       }
     }
   } else {
-    console.error('User ID or username not available from Telegram.');
-    throw new Error('User ID or username not available from Telegram.');
+    console.error('User ID not available from Telegram.');
+    throw new Error('User ID not available from Telegram.');
   }
+
+  return tgUserID;
 };
